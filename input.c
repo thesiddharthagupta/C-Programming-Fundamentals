cca977e5e1b85e269eb77a3f84768b2168a19edf
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-28 23:48:04 +0530

    Input taken

---
+++ input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    printf("Enter the value of b: ");
+    scanf("%d",&b);
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %d", b);
+
+    return 0;
+}
