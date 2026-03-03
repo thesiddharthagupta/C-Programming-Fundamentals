7218b858b83879beb31f39bf3522405ee69c9c10
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-03 15:20:04 +0530

    Check Digits

--- Basic/Check_digit.c
+++ Basic/Check_digit.c
@@ -4,10 +4,21 @@ int main(){
     printf("Enter a Character: ");
     scanf(" %c",&ch);
 
+    // Using ASCII value
     if (ch >= '0' && ch <= '9'){
         printf("It is a digit.");
     } else{
         printf("It is Not a Digit.");
+        printf("It is Not Single Digit number or May be charactor")
     }
     return 0;
 }
+
+/* // by isdigit function
+
+if(isdigit(ch)) {
+        printf("It is a digit.");
+    } else {
+        printf("It is not a digit.");
+        
+*/
