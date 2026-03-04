e5fff1ddce2495ec4a9b8b87b8853add184932da
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-04 18:09:55 +0530

    Positive negative number

---
+++ Basic/pos_neg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter num to know pos/neg: ");
+    scanf("%d",&num);
+
+    if (num>0){
+        printf("The number is Positive!");
+    }
+    else if (num == 0)
+    {
+        printf("The number is Zero!");
+    }
+    
+    else {
+        printf("The number is Negative!");
+    }
+return 0;
+}
