95da621d90caa74f655f8657e775a90ddcd0242d
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-05 01:16:37 +0530

    Grade inditify

---
+++ Conditional_Statements/grade.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("Enter Total marks: ");
+    scanf("%d",&marks);
+
+    if (marks < 30){
+        printf("grade is 'C'\n");
+    } else if (30 <= marks && marks < 70)
+    {
+        printf("Grade is 'B'\n");
+    } else if (70 <= marks && marks < 90)
+    {
+        printf("grade is 'A'\n");
+
+    } else if (marks >= 90 && marks <= 100)
+    {
+        printf("Grade is 'A+'\n");
+    } else {
+        printf("Invalid Marks!\n");
+    }
+
+    return 0;
+    
+}
