d6ed0611c92d79341b6be077762ebbed925f3843
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-05 17:09:49 +0530

    Sizeof keyword

--- Basic/sizeofKeyword.c
+++ Basic/sizeofKeyword.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+
 int main(){
-    
-    printf("The Size of int is %d\n", sizeof(int));
-    
+
+    printf("Size of int = %d\n", sizeof(int));
+    printf("Size of int pointer = %d\n", sizeof(int*));
+    printf("Size of float = %d\n", sizeof(float));
+    printf("Size of char = %d\n", sizeof(char));
+
     return 0;
 }
