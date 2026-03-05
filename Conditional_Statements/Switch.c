bc2dca94060b4ebfca6d87eb1126c34b75c2c152
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-05 23:36:46 +0530

    Switch cases on char

--- Conditional_Statements/Switch.c
+++ Conditional_Statements/Switch.c
@@ -22,3 +22,27 @@ int main(){
 
     return 0;
 }
+
+
+// #include<stdio.h>
+// #include<math.h>
+// int main() {
+// char day = 'f';
+// switch(day) {
+// case 'm' : printf("monday \n");
+// break;
+// case 't' : printf("tuesday \n");
+// break;
+// case 'w' : printf("wednesday \n");
+// break;
+// case 'T' : printf("thursday \n");
+// break;
+// case 'f' : printf("friday \n");
+// break;
+// case 's' : printf("saturday \n");
+// break;
+// case 'S' : printf("sunday \n");
+// break;
+// }
+// return 0; 
+// }
