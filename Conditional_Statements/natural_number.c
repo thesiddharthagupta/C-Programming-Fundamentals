d6bf0b02e5ba32e9548615728cd55d1fe7ed4522
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-06 22:00:07 +0530

    finding natural number

---
+++ Conditional_Statements/natural_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a Number: ");
+    scanf("%d",&num);
+    if (num>=1){
+        printf("Entered number are Natural Number!");
+    }
+    else {
+        printf("Entered Number are Not natural Number!");
+    }
+    return 0;
+
+
+}
