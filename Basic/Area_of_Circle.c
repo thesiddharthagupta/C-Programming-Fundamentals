0635bc33ac5c5962398954dbbcbcc00589151b31
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-10 23:35:04 +0530

    Area of Circle

--- Basic/Area_of_Circle.c
+++ Basic/Area_of_Circle.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
-int main(){
-    // 3.14 * r*r = area of circle
-    float pie = 3.14;   // use can directly assign value at print too.
-    float radius;
-    printf("Enter the radius: ");
-    scanf("%f" ,&radius);
 
-    printf("The Area of Circle is: %.2f", pie*radius*radius);    //%.2f prints two decimal place
+int main(){
+    float pie = 3.14;
+    float radius, area, circumference;
+
+    printf("Enter the radius: ");
+    scanf("%f", &radius);
+
+    area = pie * radius * radius;
+    circumference = 2 * pie * radius;
+
+    printf("Area of Circle: %.2f", area);
+    printf("\nCircumference of Circle: %.2f", circumference);
+
     return 0;
 }
