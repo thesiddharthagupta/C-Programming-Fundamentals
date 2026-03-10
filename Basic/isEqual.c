b6c48d76d7e02aed360f4b7b214fb45061d1b38d
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-10 21:43:50 +0530

    IS EQUAL or NOT?

---
+++ Basic/isEqual.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter TWO number to compare: ");
+    scanf("%d%d",&a,&b);
+
+    (a==b)? printf("entered numbers are Equal") : printf("Entered number are Not Equal");
+    return 0;
+
+}
