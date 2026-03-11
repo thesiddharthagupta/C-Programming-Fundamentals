9f7d4b6020177e9a30211ed0ce0c907993bce9c4
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-11 21:32:51 +0530

    Finding Cube number

--- Basic/cube.c
+++ Basic/cube.c
@@ -4,6 +4,6 @@ int main(){
     printf("Enter cube area: ");
     scanf("%d",&a);
 
-    printf("The area of cube is: %d", a*a*a);
+    printf("The area of cube is: %d", a*a*a);  // pow(a,3) with math.h
     return 0;
 }
