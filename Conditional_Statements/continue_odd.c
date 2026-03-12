08948bbe8a8ec5eadefa72ae91be640028c9772a
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-12 22:49:30 +0530

    ex. Continue for odd

--- Conditional_Statements/continue_odd.c
+++ Conditional_Statements/continue_odd.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-int main(){
+int main()
+{
     int i = 1;
-    while(i <= 10){
+    while(i <= 10)  
+    {
         if (i % 2 == 0){
             i++;        // increment before continue
             continue;
