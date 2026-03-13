b9764eae6df03ca299e4b75883fd0332edb28dde
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-13 22:17:22 +0530

    Char relation

---
+++ Conditional_Statements/char_relation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int a = 100;
+    if (a > 30)
+        printf("Demon");
+    else if (a > 20)
+        printf("Alish");
+    else if (a > 10)
+        printf("Yesh");
+    else
+        printf("Nothing");
+}
