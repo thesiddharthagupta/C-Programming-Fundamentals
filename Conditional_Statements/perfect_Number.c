39fcf65865cbd4b1aba5a84eb722ed988d7f266c
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-16 23:28:41 +0530

    iddentify Perfect number

---
+++ Conditional_Statements/perfect_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int num, i, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    for(i = 1; i <= num/2; i++){
+        if(num % i == 0){
+            sum = sum + i;
+        }
+    }
+
+    if(sum == num) {
+        printf("Perfect Number");
+    }
+    else {
+        printf("Not a Perfect Number");
+    }
+    return 0;
+}
