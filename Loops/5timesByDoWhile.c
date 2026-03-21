fc7e9616c884e7095d771b495cdbb1d28e327e92
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-21 16:42:24 +0530

    5 times printing by do while

---
+++ Loops/5timesByDoWhile.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char ch[100];
+    int i = 1;
+    printf("ENter your name: ");
+    scanf("%s",ch);    //char array doesnot required &
+
+    do{
+        printf("You Entered name is: %s\n",ch);
+        i++;  // increment
+    } 
+    while (i<5);
+    return 0;
+    }
