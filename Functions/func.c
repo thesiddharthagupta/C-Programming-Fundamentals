7aecadb0a75778c1f9f2f19ee191568a40489bc6
thesiddharthagupta <thesiddgupta@gmail.com>
2026-03-26 23:20:36 +0530

    about function

---
+++ Functions/func.c
@@ -0,0 +1,21 @@
+//  about func:- 
+// block of code which perform specific tasks.
+// increase reusability
+
+
+#include<stdio.h>
+// declaration/prototype
+void printHello();  
+
+int main(){
+    //function call
+    printHello();  
+    printHello();  // functioncall2
+    return 0;
+}
+
+// function defination
+void printHello(){
+    printf("Hello There!\n");
+}
+
