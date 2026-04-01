aa7d203d48d93050a93502788c73f7dfc14c3f7e
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-01 23:17:56 +0530

    Float example

---
+++ Functions/float.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void calculateprice(float value);
+int main() {
+    float value = 100.0;
+    calculateprice(value);
+    return 0;
+}
+
+void calculateprice(float value){
+    value = value + (0.18 * value);
+    printf("the value is : %f", value );
+}
+
+
+
