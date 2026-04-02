be54aaf9f56b8d7503a154dd8ee7028b992729c0
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-02 22:15:17 +0530

    greater amg two number

---
+++ Functions/greaterAmgTwo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int greater(int a, int b){
+    if (a > b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main(){
+    int num,num2;
+    printf("Enter Two Numbers: ");
+    scanf("%d%d",&num,&num2);
+
+    int result = greater(num,num2);
+    printf("%d is the greater number.\n", result);
+    return 0;
+}
