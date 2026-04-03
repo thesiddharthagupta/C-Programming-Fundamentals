7e6ee420c7be15391dbd4ee11b6f1bd3c2f1382f
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-03 23:17:46 +0530

    pos, neg, zero?

---
+++ Functions/isPositive.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int IsPositive(int num){
+    if (num>0){
+        return 1;
+    } else if (num < 0){
+            return -1;
+    } else {
+        return 0;
+    }
+}
+
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    
+    // we can store IsPositive = result and use result instend of calling same function twise.
+    if (IsPositive(num) == 1){
+        printf("Positive!");
+    } else if (IsPositive(num) == -1){
+        printf("Negative");
+    } else {
+        printf("Zero");
+    }
+    return 0;
+}
