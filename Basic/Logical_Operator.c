1c1e174200a1e3270e69725a9339912c5592a5cb
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-08 23:14:30 +0530

    logical operator

---
+++ Basic/Logical_Operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+
+// AND "&&" operator
+printf("%d \n", 3<4 && 3<5);  //-> True
+printf("%d \n", 3<4 && 5<4);  //-> False (0)
+
+// OR "||" operator
+printf("%d \n", 3<4 || 5<4);    //-> True (1)
+printf("%d \n", 3>4 || 5>4);    //-> True (1)
+printf("%d \n", 3<4 || 3<5);    //-> True (1)
+
+//Not "!"  operator
+printf("%d \n", !(3<4 && 3<5)); //-> False (0)
+printf("%d \n", !(4<3 || 5<3)); //-> True
+return 0;
+}
+
