7b288c287956fe4f63a2b917b73eafc2833b74dc
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-15 21:06:31 +0530

    ex. for printing by do-while loop

--- Loops/printsByDoWhile.c
+++ Loops/printsByDoWhile.c
@@ -3,7 +3,7 @@ int main(){
     char ch[100];
     int i = 1;
     printf("ENter your name: ");
-    scanf("%s",ch);    //char array doesnot required &
+    scanf("%s",ch);    //char array doesnot required "&"
 
     do{
         printf("You Entered name is: %s\n",ch);
