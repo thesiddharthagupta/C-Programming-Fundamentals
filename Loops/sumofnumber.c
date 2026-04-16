b443923638a158c37273fee50391f9226173d60d
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-16 20:59:22 +0530

    sum of n- number

---
+++ Loops/sumofnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int sum=0,n;
+    printf("ENter a numbers: ");
+    scanf("%d",&n);
+
+    for(int i = 1; i<=n;i++){
+        sum += i;
+        printf("sum of number is: %d", sum);
+
+        return 0;
+    }
+}
