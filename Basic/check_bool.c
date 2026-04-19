bf12905b1e4ae51e7708ff767dab827903263073
thesiddharthagupta <thesiddgupta@gmail.com>
2026-04-19 23:02:12 +0530

    Check the boolean

--- Basic/check_bool.c
+++ Basic/check_bool.c
@@ -2,7 +2,7 @@
 int main(){
     // int  a,b;
     
-    printf("%d \n", 4>3);
-    printf("%d \n", 3>4);
+    printf("%d \n", 4>3);   // True -> 1
+    printf("%d \n", 3>4);   // False -> 0
 return 0;    
 }
