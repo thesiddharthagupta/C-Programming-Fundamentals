154a2a8da14969b5f85dbdff52e2aaddc53a97ee
Siddharth Gupta <thesiddgupta@gmail.com~>
2026-04-20 15:09:15 +0530

    ex. of finding cube

--- Basic/cube.c
+++ Basic/cube.c
@@ -4,6 +4,6 @@ int main(){
     printf("Enter a number: ");
     scanf("%d",&a);
 
-    printf("The cube of the number is: %d\n", a*a*a);  // pow(a,3) with math.h
+    printf("The cube of the number is: %d\n", a*a*a);  // pow(a,3) with math.h - headerFile
     return 0;
 }
