d2e08cd39030335d8b44298b38489f471b97b2b0
Siddharth Gupta <thesiddgupta@gmail.com~>
2026-04-20 14:46:34 +0530

    example of cube

--- Basic/cube.c
+++ Basic/cube.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int a;
-    printf("Enter cube area: ");
+    printf("Enter a number: ");
     scanf("%d",&a);
 
-    printf("The area of cube is: %d", a*a*a);  // pow(a,3) with math.h
+    printf("The cube of the number is: %d\n", a*a*a);  // pow(a,3) with math.h
     return 0;
 }
