5cccb225a80bf4866fffa634006370b44f0caec3
Siddharth Gupta <thesiddgupta@gmail.com>
2026-04-21 17:46:03 +0530

    Added relational operator program

---
+++ Basic/Relational_operator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+//  Assign
+printf("%d \n", 4==4);
+
+// less
+printf("%d \n", 4<3);
+printf("%d \n", 3<4);
+printf("%d \n", 4<4);
+printf("%d \n", 4<=4);
+
+// greater
+printf("%d \n", 4>3);
+printf("%d \n", 3>4);
+printf("%d \n", 4>4);
+printf("%d \n", 4>=4);
+
+// not 
+printf("%d \n", 4!=4);
+printf("%d \n", 3!=4);
+
+return 0;
+
+}
