8953dd08f4918f6c8297cc35cf8f039023b14e0e
Siddharth Gupta <thesiddgupta@gmail.com>
2026-04-24 22:47:11 +0530

    fibonacci series

---
+++ Functions/fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fibo(int n){
+    if (n==0)
+    return 0;
+    else if (n==1)
+    return 1;
+    else {
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+int main(){
+    int n;
+    printf("Enter number of terms: ");
+    scanf("%d",&n);
+    for (int i=1; i <=n; i++){
+        printf("%d\t",fibo(i));
+
+    }
+    return 0;
+}
