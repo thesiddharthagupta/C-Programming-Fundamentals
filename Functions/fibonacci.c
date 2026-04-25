271391339647c250bfb290e00c8838bc0588ddbe
Siddharth Gupta <thesiddgupta@gmail.com>
2026-04-25 23:18:49 +0530

    Fibonacci series

--- Functions/fibonacci.c
+++ Functions/fibonacci.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int fibo(int n){
     // base cases 
-    if (n==0)   
+    if (n==0)   // cond 1
     return 0;
-    else if (n==1)
+    else if (n==1)  // cond 2
     return 1;
     else {
         return fibo(n-1)+fibo(n-2);
