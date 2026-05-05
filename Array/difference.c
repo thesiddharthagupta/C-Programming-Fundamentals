50ef43934423b301c79b41dd2a22d8dadea3931b
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-05 23:37:05 +0530

    DIFFERENCE IN array

---
+++ Array/difference.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int age = 22;
+    int _age = 23;
+    int *ptr = &age;
+    int *_ptr = &_age;
+    printf("%u, %u difference = %u\n", ptr,_ptr,ptr -_ptr);
+    // difference is 1 int data type = 4 bytes.
+    return 0;
+}
