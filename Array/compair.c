28fde4c9a09248f2b3164781e57991eb63e3973d
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-05 23:42:03 +0530

    Compairing array

---
+++ Array/compair.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int age = 22;
+    int _age = 23;
+    int *ptr = &age;
+    int *_ptr = &_age;
+    printf("%u, %u difference = %u\n", ptr,_ptr,ptr -_ptr);
+    // difference is 1 int data type = 4 bytes.
+    
+    _ptr = &age;
+    printf("Comparision = %u\n", ptr == _ptr);
+    // output 1 because its true
+    return 0;
+}
