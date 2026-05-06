6564e7f491cb18aefabc1e2c20487aabf5704142
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-06 23:08:33 +0530

    indexing array

---
+++ Array/index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int card[5];
+    int *ptr = &card[0];
+
+    // Input values
+    for (int i = 0; i < 5; i++) {
+        printf("%d index: ", i);
+        scanf("%d", (ptr + i));
+    }
+
+    // Display values
+    for (int i = 0; i < 5; i++) {
+        printf("%d index = %d\n", i, card[i]);
+    }
+
+    return 0;
+}
