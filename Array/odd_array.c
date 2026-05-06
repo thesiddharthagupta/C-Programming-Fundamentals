13b4b7edc981d343f3868ab17c25e4f68e5fbf9c
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-06 23:18:23 +0530

    odd by array

---
+++ Array/odd_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int countodd(int arr[], int n){
+    int count = 0;
+    for(int i = 0; i<n; i++){
+        if (arr[i] % 2 != 0){
+            count ++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    printf("%d", countodd (arr,6));     // 6 is the size of array
+    // printf("%d", sizeof(arr) / sizeof(arr[0]));
+    return 0;
+}
+
