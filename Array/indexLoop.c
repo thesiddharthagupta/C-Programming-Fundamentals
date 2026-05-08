81900153f296b1113350e19557c966d8b519fd56
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-08 21:53:00 +0530

    indexing using loops

--- Array/indexLoop.c
+++ Array/indexLoop.c
@@ -2,8 +2,8 @@
 int main(){
     int arr[] = {1,1,2,3,5,8,13,21};
 
-    for (int i = 1; i < 8; i++){
-        printf("%d\n",arr[i]);
+    for (int i = 0 ; i < 8; i++){       // starting loops from 0;
+        printf("%d\n",arr[i]);          // iteration 
     }
     return 0;
 }
