71161b4dc6905e3b0f0c392b162ca9c04067bd08
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-08 21:11:26 +0530

    array indexing

---
+++ Array/appendingArray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[5] = {1,5,9,12,17};
+
+    printf("%d\n",arr[0]);
+    printf("%d\n",arr[1]);
+    printf("%d\n",arr[2]);
+    printf("%d\n",arr[3]);
+    printf("%d\n",arr[4]);
+        
+    }
