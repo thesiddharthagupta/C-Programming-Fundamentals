86128f5462a836462ad50def7797a3eda87f9570
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-11 23:41:19 +0530

    Compair number

--- Basic/Compair.c
+++ Basic/Compair.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
     int a = 5,b = 4;
-    (a==b)? printf("same") : printf("not same");
-
+    (a==b)? printf("same") : printf("not same");    //ternary operator
+    if(a==b)
+        printf("same");
     return 0;
 
 }
