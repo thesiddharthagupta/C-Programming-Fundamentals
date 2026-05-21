02bee30787a82deeb6d76d9f978d69897a09ba4f
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-21 23:09:31 +0530

    largest number amg Array

--- Array/largest.c
+++ Array/largest.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    int arr[5] = {4,8,1,9,2};
+    int arr[5] = {4,8,1,9,2};   // array input 
     int max = arr[0];
     for(int i = 1; i < 5; i++){
-        if (arr[i] > max) {
+        if (arr[i] > max) {     // condn 
             max = arr[i];
 
         }
