9a81335f147eaa408c3bda0ea8fd2b97e96a84c9
Siddharth Gupta <thesiddgupta@gmail.com>
2026-05-31 18:54:14 +0530

    ex. of structure

---
+++ Structures/structure.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+struct student{
+    char name[100];
+    int roll;
+    float Cgpa;
+};
+
+int main(){
+    struct student s1;
+    s1.roll = 1664;
+    s1.Cgpa = 9.2;
+    strcpy(s1.name, "Siddhartha");
+
+    printf("Student name = %s\n", s1.name);
+    printf("Student rollNo. = %d\n", s1.roll);
+    printf("Student Cgpa = %f\n", s1.Cgpa);
+    
+    return 0;
+}
+
+
