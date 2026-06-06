1a0218308ade2b8d5a3b426646bd2aa6890a36cb
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-06 23:03:04 +0530

    Bobble shot

--- Conditional_Statements/Bobble_shot.c
+++ Conditional_Statements/Bobble_shot.c
@@ -7,13 +7,13 @@ int main() {
     scanf("%d", &n);
 
     printf("Enter elements:\n");
-    for (i = 0; i < n; i++) {   // Fix 1
+    for (i = 0; i < n; i++) {   // loop on n 
         scanf("%d", &arr[i]);
     }
 
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {   // Fix 2
+            if (arr[j] > arr[j + 1]) {   //loop on j + 1 /  n - i - 1 is for sorting 
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
