03fb31e6c9ee4ba162ffa0f4c18c3e2ce5efb131
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-07 22:38:59 +0530

    ex. of break continue

--- Conditional_Statements/break_continue.c
+++ Conditional_Statements/break_continue.c
@@ -5,9 +5,9 @@ int main(){
     // scanf("%d",&num);
 
     for (int i=1; i<=20; i++){
-        if (i==8){
+        if (i==8){          // terminate itteration at .8
             break;
-        } else if (i==3){
+        } else if (i==3){   // skips 3 on iteration
             continue;
         }
         printf("%d",i);
