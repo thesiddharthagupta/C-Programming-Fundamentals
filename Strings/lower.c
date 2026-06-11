db5fef8943ce88c94018cfbf44960abf912ff30a
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-11 22:14:52 +0530

    Lower

---
+++ Strings/lower.c
@@ -0,0 +1,6 @@
+a. Write a program to convert all lowercase vowels to uppercase in a string.  
+
+
+b. Write a program to print the highest frequency character in a string.  
+c. Write a program to remove blank spaces in a string.  
+d. Write a program to replace lowercase letters with uppercase & vice versa in a string.
