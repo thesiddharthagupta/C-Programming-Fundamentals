f0905b902b756a75bb3ef23caf37c8cbddbc5362
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-16 23:41:02 +0530

    largest number

---
+++ Pointer/largestnumber.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void findElements(int arr[], int n, int *largest, int secoundlarges, int smallest) {
+    *largest = *secoundlargest = arr[0];
+    *smallest = arr[0];
+    for (int i = 0; i < n; i++){
+        if arr[i] < smallest
+    }
+}
