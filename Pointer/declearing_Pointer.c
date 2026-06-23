d36aabd677830bc136cb295b490f3cda14c716c7
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-23 23:54:33 +0530

    declearation of pointer

--- Pointer/declearing_Pointer.c
+++ Pointer/declearing_Pointer.c
@@ -2,7 +2,7 @@
 int main()
 {
 int age = 22;
-int *ptr = &age;    // pointing to age. & wer use pointer
+int *ptr = &age;    // pointing to age. & for use pointer
 int _age = *ptr;
 
 printf("age is %d", *ptr);
