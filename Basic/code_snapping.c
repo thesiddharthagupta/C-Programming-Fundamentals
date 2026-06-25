9e0249b133205746dc6be7eb2d82024f27284ebe
Siddharth Gupta <thesiddgupta@gmail.com>
2026-06-25 22:57:36 +0530

    globle_local var

--- Basic/code_snapping.c
+++ Basic/code_snapping.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
- int a = 10;
-void m(){
-        int a = 5;
-        a++;
-        printf("%d",a);
-    }
- int main(){
+
+int a = 10;     // Global variable
+
+void m()
+{
+    int a = 5;  // Local variable
+    a++;        // Increment local variable
+    printf("%d", a);
+}
+
+int main()
+{
     m();
-    printf("%d",a);
+    printf("%d", a);
     return 0;
-    }
-    
- 
+}
