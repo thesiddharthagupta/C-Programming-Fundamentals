3664f6960ff4f59a59d41ebf2bad79b82c4d5012
Siddharth Gupta <thesiddgupta@gmail.com>
2026-07-02 23:17:47 +0530

    str copy string

--- Strings/str_cpy.c
+++ Strings/str_cpy.c
@@ -5,6 +5,8 @@ int main(){
     char newstr[] = "newstr";
     strcpy (newstr, oldstr);
     puts(newstr);
+
+    return 0;
 }
 
 //its uses as copying from oldstr to new and value are stored in newstr by coping from oldstr.
