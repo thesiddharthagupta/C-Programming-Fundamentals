601e4ed544d2c3f4aebf2df5609b105157a412de
Siddharth Gupta <thesiddgupta@gmail.com>
2026-07-04 22:25:43 +0530

    Prime number

---
+++ Functions/prime.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int isprime(int num){
+    int count = 0;
+
+    for(int i = 1; i <= num; i++){
+        if (num % i == 0){
+            count++;
+        }
+    }
+
+    if (count == 2)
+        return 1;
+    else
+        return 0;
+}
+
+int main(){
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(isprime(num)){
+        printf("%d is Prime Number!", num);
+    } else {
+        printf("%d is Not a Prime Number!", num);
+    }
+
+    return 0;
+}
