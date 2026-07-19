2f498905048312dbbd582a626e40666d700b9b79
Siddharth Gupta <thesiddgupta@gmail.com>
2026-07-19 23:31:26 +0530

    All Logical-Operators.

--- Basic/Logical_Operator.c
+++ Basic/Logical_Operator.c
@@ -2,7 +2,7 @@
 int main() {
 
 // AND "&&" operator
-printf("%d \n", 3<4 && 3<5);  //-> True
+printf("%d \n", 3<4 && 3<5);  //-> True (1)
 printf("%d \n", 3<4 && 5<4);  //-> False (0)
 
 // OR "||" operator
@@ -10,9 +10,10 @@ printf("%d \n", 3<4 || 5<4);    //-> True (1)
 printf("%d \n", 3>4 || 5>4);    //-> True (1)
 printf("%d \n", 3<4 || 3<5);    //-> True (1)
 
-//Not "!"  operator
-printf("%d \n", !(3<4 && 3<5)); //-> False (0)
-printf("%d \n", !(4<3 || 5<3)); //-> True
+//Not "!" operator
+printf("%d \n", !(3<4 && 3<5));     //-> False (0)
+printf("%d \n", !(4<3 || 5<3));     //-> True
+
 return 0;
 }
 
