87e3a294c1b87bcc6700a147bde874177e8a4730
Siddharth Gupta <thesiddgupta@gmail.com>
2026-07-21 23:42:40 +0530

    number decleration

--- Basic/num.c
+++ Basic/num.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b;        //decleration 
     printf("Enter any number: ");
-    scanf("%d%d",&a, &b);
+    scanf("%d%d",&a, &b);       // input
     printf("a=%d\n b=%d", a,b);
     return 0;
 }
