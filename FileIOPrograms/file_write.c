d43fdd70f9b3bbcfa06b8bdb622f9e982a81b98c
Siddharth Gupta <thesiddgupta@gmail.com>
2026-08-01 21:44:11 +0530

    File writting by file io

--- FileIOPrograms/file_write.c
+++ FileIOPrograms/file_write.c
@@ -2,18 +2,24 @@
 int main(){
     FILE *fptr;
     fptr = fopen("student.txt","w");
+
     char name[100];
     int age;
     float cgpa;
-    printf("Enter name: ");     // get
-    scanf("%s",&name);          // put
+
+    printf("Enter name: ");     
+    scanf("%s",&name);          
+
     printf("Enter age: ");
     scanf("%d",&age);
+
     printf("Enter Cgpa: ");
     scanf("%f",&cgpa);
-    fprintf(fptr,"%s\t",name);
-    fprintf(fptr,"%d\t",age);
-    fprintf(fptr,"%f",cgpa);
+
+    fprintf(fptr, "%s\t%d\t%.2f", name, age, cgpa);
     fclose(fptr);
     return 0;
 }
+
+
+//cvgbhnj
